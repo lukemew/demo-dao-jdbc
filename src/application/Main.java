f49c056f6d027d4ac5cc8dca5534a846ea111f83
lukemew <lucasmelor65@gmail.com>
2024-10-03 15:57:57 -0300

    Reusing Instantiation

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: Seller findById() ===");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
